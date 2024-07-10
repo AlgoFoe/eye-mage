a2f365e2093623a7fca85841d7d3f20a0ec48abb
AlgoFoe <varun.singh10011@gmail.com>
2024-07-10 12:29:05 +0530

    added clerk folder

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(["/", "/credits(.*)"]);
-const isIgnoredRoute = createRouteMatcher(["/api/webhooks(.*)"]);
+// Apply Clerk middleware to protected routes
+export default clerkMiddleware()
 
-export default clerkMiddleware((auth, req) => {
-  if (isIgnoredRoute(req)) {
-    // If the route is in the ignored list, skip the middleware
-    return;
-  }
-  if (isProtectedRoute(req)) {
-    // Protect the route if it is in the protected list
-    auth().protect();
-  }
-});
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    '/((?!.*\\..*|_next|api/webhooks/clerk|api/webhooks/stripe|$).*)', // Exclude static files, _next, and the specified webhook routes
+    '/(api|trpc)(.*)',
+  ],
+}
